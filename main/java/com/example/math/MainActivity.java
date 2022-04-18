a0851f5ca739d6b84407a799d691c88c4f85a96f
7x7x49 <98600734+7x7x49@users.noreply.github.com>
2022-04-18 08:21:56 +0300

    Add files via upload

---
+++ main/java/com/example/math/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.math;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    EditText editText1;
+    Integer firstNumber = 0;
+    Integer secondNumber = 0;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intent = new Intent(this, Result.class);
+        intent.putExtra("firstNumber", firstNumber);
+        intent.putExtra("secondNumber", secondNumber);
+        editText = findViewById(R.id.editText);
+        editText1 = findViewById(R.id.editText1);
+
+    }
+
+    public void onClickNegative(View view){
+        try {
+            firstNumber = Integer.parseInt(editText.getText().toString());
+            if (firstNumber > 0) {
+                editText.setText("-" + firstNumber);
+            }
+            if (firstNumber < 0) {
+                firstNumber *= -1;
+                editText.setText(firstNumber.toString());
+            }
+        }
+        catch (Exception ex){
+            Toast.makeText(this, "Попробуйте снова", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickNegative1(View view){
+        try {
+            secondNumber = Integer.parseInt(editText1.getText().toString());
+            if (secondNumber > 0) {
+                editText1.setText("-" + secondNumber);
+            }
+            if (secondNumber < 0) {
+                secondNumber *= -1;
+                editText1.setText(secondNumber.toString());
+            }
+        }
+        catch (Exception ex){
+            Toast.makeText(this, "Попробуйте снова", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickSum(View view){
+        try {
+            firstNumber = Integer.parseInt(editText.getText().toString());
+            secondNumber = Integer.parseInt(editText1.getText().toString());
+            intent.putExtra("firstNumber", firstNumber);
+            intent.putExtra("secondNumber", secondNumber);
+            startActivity(intent);
+        }
+        catch (Exception ex){
+            Toast.makeText(this, "Попробуйте снова", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ main/java/com/example/math/Result.java
@@ -0,0 +1,36 @@
+package com.example.math;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Bundle arguments = getIntent().getExtras();
+        TextView textView2 = findViewById(R.id.textView2);
+        String firstNumber;
+        String secondNumber;
+        Integer result = arguments.getInt("firstNumber")+arguments.getInt("secondNumber");
+
+        firstNumber = arguments.get("firstNumber").toString();
+
+        if(arguments.getInt("secondNumber")<0){
+            secondNumber = "("+arguments.get("secondNumber").toString()+")";
+        }
+        else {
+            secondNumber = arguments.get("secondNumber").toString();
+        }
+        textView2.setText(firstNumber+" + "+secondNumber+" = "+result);
+    }
+    public void onClickReturn(View view){
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+    }
+}
